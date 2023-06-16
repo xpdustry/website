352bfb29775a0f3c216881fe92953871d7a6e37a
luxluth <delphin.blehoussi93@gmail.com>
2023-06-16 16:22:17 +0200

    Front page added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="top">
+    <h1>Xpdustry</h1>
+    <p><em>The first and only Mindustry French server since 2019</em></p>
+</div>
+
+<style>
+    .top {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .top h1 {
+        font-family: var(--xpd-font);
+        font-size: clamp(3.75rem, 2.1875rem + 5vw, 5rem);
+    }
+</style>
