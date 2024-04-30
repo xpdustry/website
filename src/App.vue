157d0689e3eb24cea226b4c3ae5a70f35fccde3c
phinner <62483793+phinner@users.noreply.github.com>
2024-04-30 03:52:09 +0200

    fix: Revert eslint version

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,30 @@ import IconXPDark from '@/components/icons/IconXPDark.vue'
 </script>
 
 <template>
-	<div class="p-4 h-screen bg-gradient-to-tl from-cyan-500 to-green-500 text-gray-700">
-		<header class="flex flex-row w-full">
-			<nav class="basis-1/2 flex flex-row">
-				<RouterLink class="rounded-lg bg-white drop-shadow-lg w-12 h-12 m-2 p-2" to="/">
-					<IconXPDark class="fill-cyan-400"/>
-				</RouterLink>
-			</nav>
-			<div class="basis-1/2 flex flex-row-reverse">
-				<a class="rounded-lg bg-white drop-shadow-lg w-12 h-12 m-2 p-2" href="https://discord.xpdustry.fr">
-					<IconDiscord class="fill-cyan-400"/>
-				</a>
-				<a class="rounded-lg bg-white drop-shadow-lg w-12 h-12 m-2 p-2" href="https://github.com/Xpdustry">
-					<IconGithub class="fill-cyan-400"/>
-				</a>
-			</div>
-		</header>
-		<main>
-			<RouterView />
-		</main>
-	</div>
+  <div class="p-4 h-screen bg-gradient-to-tl from-cyan-500 to-green-500 text-gray-700">
+    <header class="flex flex-row w-full">
+      <nav class="basis-1/2 flex flex-row">
+        <RouterLink class="rounded-lg bg-white drop-shadow-lg w-12 h-12 m-2 p-2" to="/">
+          <IconXPDark class="fill-cyan-400" />
+        </RouterLink>
+      </nav>
+      <div class="basis-1/2 flex flex-row-reverse">
+        <a
+          class="rounded-lg bg-white drop-shadow-lg w-12 h-12 m-2 p-2"
+          href="https://discord.xpdustry.fr"
+        >
+          <IconDiscord class="fill-cyan-400" />
+        </a>
+        <a
+          class="rounded-lg bg-white drop-shadow-lg w-12 h-12 m-2 p-2"
+          href="https://github.com/Xpdustry"
+        >
+          <IconGithub class="fill-cyan-400" />
+        </a>
+      </div>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,8 @@
 <template>
-	<div class="m-4 h-80 flex items-center text-center">
-		<div class="w-full">
-			<h1 class="font-audiowide text-4xl md:text-8xl justify-center m-4">
-				Xpdustry
-			</h1>
-			<p class="italic text-xl">
-				The first Mindustry French server since 2019
-			</p>
-		</div>
-	</div>
+  <div class="m-4 h-80 flex items-center text-center">
+    <div class="w-full">
+      <h1 class="font-audiowide text-4xl md:text-8xl justify-center m-4">Xpdustry</h1>
+      <p class="italic text-xl">The first Mindustry French server since 2019</p>
+    </div>
+  </div>
 </template>
