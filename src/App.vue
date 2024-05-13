b83df8d8472122cd5d61efc3d6498507b4754812
phinner <62483793+phinner@users.noreply.github.com>
2024-05-13 11:42:58 +0200

    feat: Implemented server status (#13)

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,22 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { SpeedInsights } from '@vercel/speed-insights/vue';
-import IconDiscord from '@/components/icons/IconDiscord.vue'
-import IconGithub from '@/components/icons/IconGithub.vue'
-import IconXPDark from '@/components/icons/IconXPDark.vue'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
+import TopNavigation from '@/components/home/TopNavigation.vue'
 </script>
 
 <template>
-	<SpeedInsights/>
-  <div class="p-4 h-screen bg-gradient-to-tl from-cyan-500 to-green-500 text-gray-700">
-    <header class="flex flex-row w-full">
-      <nav class="basis-1/2 flex flex-row">
-        <RouterLink class="rounded-lg bg-white drop-shadow-lg w-12 h-12 m-2 p-2" to="/">
-          <IconXPDark class="fill-cyan-400" />
-        </RouterLink>
-      </nav>
-      <div class="basis-1/2 flex flex-row-reverse">
-        <a
-          class="rounded-lg bg-white drop-shadow-lg w-12 h-12 m-2 p-2"
-          href="https://discord.xpdustry.fr"
-        >
-          <IconDiscord class="fill-cyan-400" />
-        </a>
-        <a
-          class="rounded-lg bg-white drop-shadow-lg w-12 h-12 m-2 p-2"
-          href="https://github.com/Xpdustry"
-        >
-          <IconGithub class="fill-cyan-400" />
-        </a>
-      </div>
+  <SpeedInsights />
+  <div
+    class="flex flex-col p-4 bg-gradient-to-tl from-cyan-500 to-green-500 text-gray-700 space-y-8 min-h-screen"
+  >
+    <header>
+      <TopNavigation />
     </header>
-    <main>
+    <main class="container mx-auto h-auto">
       <RouterView />
     </main>
+    <footer>
+      <p class="text-center">© 2024 Xpdustry</p>
+    </footer>
   </div>
 </template>
